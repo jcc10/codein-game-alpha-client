7d3287d0c81bf99cbaf71b36d3240d58c6d846e0
Joshua Cline <wwjdtd@gmail.com>
2019-02-26 18:16:51 -0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget -q https://gitlab.com/mr.code-joshua-cline/codein-game-alpha-client/raw/master/WS-Client.jar?inline=false -O ./WS-Client.jar
-wget -q https://gitlab.com/mr.code-joshua-cline/codein-game-alpha-client/raw/master/RunMe.sh?inline=false -O ./RunMe.sh 
+wget -q https://github.com/jcc10/codein-game-alpha-client/raw/master/WS-Client.jar -O ./WS-Client.jar
+wget -q https://raw.githubusercontent.com/jcc10/codein-game-alpha-client/master/RunMe.sh -O ./RunMe.sh 
