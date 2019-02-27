5eafe00868087e015c9adfbc93915c4c6c886d73
Joshua Cline <wwjdtd@gmail.com>
2019-02-26 16:05:57 -0800

    Fixed Scripts

---
+++ update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget -q https://gitlab.com/mr.code-joshua-cline/codein-game-alpha-client/raw/master/WS-Client.jar?inline=false -O ./WS-Client.jar
+wget -q https://gitlab.com/mr.code-joshua-cline/codein-game-alpha-client/raw/master/RunMe.sh?inline=false -O ./RunMe.sh 
